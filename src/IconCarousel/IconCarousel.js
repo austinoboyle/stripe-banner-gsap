7dda3d73b7a609434e78c2fd5e39cf7da896d4d1
austinoboyle <14arob@queensu.ca>
2018-03-10 14:01:28 -0500

    Merge stash conflict

--- src/IconCarousel/IconCarousel.js
+++ src/IconCarousel/IconCarousel.js
@@ -6,43 +6,22 @@ import styles from './IconCarousel.scss';
 export default class IconCarousel extends Component {
     constructor(props) {
         super(props);
-        let main = new TimelineMax({paused: true});
-        main.add("start");
-        this.state = {
-            numIconsLoaded: 0,
-            timeline: main
-        };
     }
 
     // This checks when all icons have loaded, and then starts the main timeline
     // Not necessarily desirable behaviour for how the animation should look, just used
     // to demonstrate the usage of the Transition callbacks functions
     checkAllLoaded() {
-        if (this.state.numIconsLoaded === this.props.icons.length) {
-            this.state.timeline.play()
-        }
     }
     
     // This is used as a callback for the IconContainer -> Transition:onEntered
     iconEntered() {
-        this.setState({numIconsLoaded: this.state.numIconsLoaded + 1}, this.checkAllLoaded);
     }
 
     render() {
-        const {icons} = this.props;
-        const {timeline} = this.state;
         return (
             <div className={styles.wrapper}>
-                {icons.map((icon, index) => (
-                    <IconContainer 
-                        image={icon.image}
-                        title={icon.title}
-                        timeline={timeline}
-                        iconEntered={() => this.iconEntered()} 
-                        startingPos={{x: icon.x, y: icon.y}}
-                        scale={icon.s}
-                    />
-                ))}
+                IconCarousel
             </div>
         );
     }

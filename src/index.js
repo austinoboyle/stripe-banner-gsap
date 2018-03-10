032dafa176c05f0cdb97f1b3f59e46dbd2bed910
austinoboyle <14arob@queensu.ca>
2018-03-10 13:15:56 -0500

    Move app.scss into index.scss global stylesheet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
+// eslint-disable-next-line
+import styles from './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

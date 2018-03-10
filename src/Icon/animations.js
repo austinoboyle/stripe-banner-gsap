cc8146300312a5896e2fb279e08c258ea0d02cd8
austinoboyle <14arob@queensu.ca>
2018-03-10 14:27:48 -0500

    Create base animations file

--- src/Icon/animations.js
+++ src/Icon/animations.js
@@ -2,25 +2,8 @@ import {TweenMax, Power0} from 'gsap';
 import {APPEAR_TIME, X_OFFSET} from '../constants';
 
 export default {
-    showLabel(target, onComplete) {
-    },
     show(target, scale, startingPos) {
-        console.log("SHOWING");
-        TweenMax.to(target, 0, {opacity: 0});
-        TweenMax.to(target, 0, {x: startingPos.x + X_OFFSET, top: startingPos.y})
-        TweenMax.fromTo(target, APPEAR_TIME, {
-            opacity: 0,
-            scale: 0.5,
-        }, {
-            opacity: 1,
-            scale: scale,
-            ease: Power0.easeNone
-        });
     },
     hide(target) {
-        TweenMax.to(target, APPEAR_TIME, {
-            opacity: 0,
-            scale: 0.5
-        })
     }
 }

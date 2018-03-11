5ab21c0973e6299a055e8ee539fb5092494c76a2
austinoboyle <14arob@queensu.ca>
2018-03-11 13:19:25 -0400

    Update utils functions to those of Master branch

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,28 @@
 /**
- * Re-scale an mappedArray to fit between two values
+ * Re-scale an Array to fit between two values
  * 
  * @export
- * @param {Array<Number>} arr
+ * @param {Array<Number|Object>} arr
  * @param {any} scaledMin new minimum value of the mappedArray 
  * @param {any} scaledMax new maximum value of the mappedArray
- * @returns {Array<Number>} re-scaled mappedArray that keeps the relative distribution
+ * @param {String} key key of object to scale if arr is an Array of Objects
+ * @returns {Array<Number|Object>} re-scaled mappedArray that keeps the relative distribution
  * of the numbers in the original mappedArray, but spaces them between the new max and min
  */
 export function scaleBetween(arr, scaledMin, scaledMax, key=null){
     let mappedArr = [...arr];
-    if (key) {
+    if (key !== null) {
         mappedArr = mappedArr.map(el => el[key]);
     }
-    const min = Math.min(mappedArr);
-    const max = Math.max(mappedArr);
+    console.log("MAPPED", mappedArr)
+    const min = Math.min(...mappedArr);
+    const max = Math.max(...mappedArr);
     if (max === min) {
         mappedArr = mappedArr.map((el) => (min + max) /2)
     } else {
         mappedArr = mappedArr.map((num) => (scaledMax-scaledMin)*(num-min)/(max-min)+scaledMin);
     }
-    if (key) {
+    if (key !== null) {
         return arr.map((el, i) => {
             el[key] = mappedArr[i];
             return el;

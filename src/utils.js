d00f6b876e07295dc8ae713bce4f842427fd6db0
austinoboyle <14arob@queensu.ca>
2018-03-11 12:48:55 -0400

    Add comments anto util functions.  Make scaleBetween more general

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,42 @@
-export const scaleBetween = (arr, scaledMin, scaledMax) => {
-    const min = Math.min(arr);
-    const max = Math.max(arr);
-    if (max === min) {
-        return arr.map((el) => (min + max) /2)
+/**
+ * Re-scale an mappedArray to fit between two values
+ * 
+ * @export
+ * @param {Array<Number>} arr
+ * @param {any} scaledMin new minimum value of the mappedArray 
+ * @param {any} scaledMax new maximum value of the mappedArray
+ * @returns {Array<Number>} re-scaled mappedArray that keeps the relative distribution
+ * of the numbers in the original mappedArray, but spaces them between the new max and min
+ */
+export function scaleBetween(arr, scaledMin, scaledMax, key=null){
+    let mappedArr = [...arr];
+    if (key) {
+        mappedArr = mappedArr.map(el => el[key]);
     }
-    return arr.map((num) => (scaledMax-scaledMin)*(num-min)/(max-min)+scaledMin);
+    const min = Math.min(mappedArr);
+    const max = Math.max(mappedArr);
+    if (max === min) {
+        mappedArr = mappedArr.map((el) => (min + max) /2)
+    } else {
+        mappedArr = mappedArr.map((num) => (scaledMax-scaledMin)*(num-min)/(max-min)+scaledMin);
+    }
+    if (key) {
+        return arr.map((el, i) => {
+            el[key] = mappedArr[i];
+            return el;
+        })
+    }
+    return mappedArr;
 }
 
-export const randomBetween = (min, max) => {
+/**
+ * Generate a random float between two values
+ * 
+ * @export
+ * @param {Number} min minimum value 
+ * @param {Number} max maximum value
+ * @returns {Number} in range [min, max)
+ */
+export function randomBetween(min, max) {
     return Math.random() * (max - min) + min;
 }
